1fac173862e4336523575a741b46672f5ee47267
0-bff <patogostosinkz1@gmail.com>
2025-07-21 20:22:27 -0300

    Update aaa.lua

--- aaa.lua
+++ aaa.lua
@@ -4,39 +4,56 @@ local Main = library:CreateWindow("BrainBlox Hub", "Crimson")
 local cheats = Main:CreateTab("Cheats")
 local misc = Main:CreateTab("Misc")
 
---// Speed Slider
+local player = game.Players.LocalPlayer
+local humanoid = nil
+
+-- Atualiza referência ao Humanoid
+local function updateHumanoid()
+    if player.Character then
+        humanoid = player.Character:FindFirstChildOfClass("Humanoid")
+    end
+end
+
+-- Atualiza sempre que o personagem reaparecer
+player.CharacterAdded:Connect(function(char)
+    wait(1) -- Espera o personagem carregar
+    updateHumanoid()
+end)
+
+-- Primeira tentativa
+updateHumanoid()
+
+--// WalkSpeed Slider
 cheats:CreateSlider("WalkSpeed", 16, 200, function(value)
-    game.Players.LocalPlayer.Character.Humanoid.WalkSpeed = value
+    if humanoid then
+        humanoid.WalkSpeed = value
+    end
 end)
 
 --// JumpPower Slider
 cheats:CreateSlider("JumpPower", 50, 300, function(value)
-    game.Players.LocalPlayer.Character.Humanoid.JumpPower = value
+    if humanoid then
+        humanoid.JumpPower = value
+    end
 end)
 
---// Infinity Jump Toggle
+--// Infinity Jump
 local infJumpEnabled = false
-
 cheats:CreateToggle("Infinity Jump", function(state)
     infJumpEnabled = state
 end)
 
--- Infinity Jump Logic
 game:GetService("UserInputService").JumpRequest:Connect(function()
-    if infJumpEnabled then
-        local humanoid = game.Players.LocalPlayer.Character:FindFirstChildOfClass("Humanoid")
-        if humanoid then
-            humanoid:ChangeState(Enum.HumanoidStateType.Jumping)
-        end
+    if infJumpEnabled and humanoid then
+        humanoid:ChangeState(Enum.HumanoidStateType.Jumping)
     end
 end)
 
--- Exemplo extra no tab Misc
-misc:CreateButton("Reset Velocidade", function()
-    local hum = game.Players.LocalPlayer.Character:FindFirstChildOfClass("Humanoid")
-    if hum then
-        hum.WalkSpeed = 16
-        hum.JumpPower = 50
+--// Reset Button
+misc:CreateButton("Reset Speed/Jump", function()
+    if humanoid then
+        humanoid.WalkSpeed = 16
+        humanoid.JumpPower = 50
     end
 end)
 
